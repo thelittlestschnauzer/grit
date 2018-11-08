e968000848a6cc6dbcd9400f63c51e3a14f8d447
Dana <doneil1580@gmail.com>
2018-11-08 11:23:28 -0500

    added room_name and description attributes to the rooms table

--- db/migrate/20181108162031_create_rooms.rb
+++ db/migrate/20181108162031_create_rooms.rb
@@ -1,7 +1,9 @@
 class CreateRooms < ActiveRecord::Migration[5.2]
   def change
     create_table :rooms do |t|
-
+      t.string :room_name
+      t.text :description
+      
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_07_235744) do
+ActiveRecord::Schema.define(version: 2018_11_08_162031) do
+
+  create_table "rooms", force: :cascade do |t|
+    t.string "room_name"
+    t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
